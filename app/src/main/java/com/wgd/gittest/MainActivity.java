75e79c3a9ec556bf0d27b44682b7c01af5d67364
“wanggd” <wangguodong@pzdf.com>
2022-11-15 11:47:29 +0800

    test git 第二次 提交

--- app/src/main/java/com/wgd/gittest/MainActivity.java
+++ app/src/main/java/com/wgd/gittest/MainActivity.java
@@ -3,10 +3,13 @@ package com.wgd.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("adadad", "onCreate: dadad a");
     }
 }
