5e775e6ed8767758fc39d63d6abf5a3a32e28f5d
“wanggd” <wangguodong@pzdf.com>
2022-11-15 14:20:19 +0800

    test git 修改了一行xml ，并且删除了一行log

--- app/src/main/java/com/wgd/gittest/MainActivity.java
+++ app/src/main/java/com/wgd/gittest/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("adadad", "onCreate: dadad a");
         Log.i("adadad", "onCreate: 修改Log");
+        Log.i("adadad", "onCreate: liuyi 添加Log");
+        Log.i("adadad", "onCreate: liuyi 修改xml");
+        Log.i("adadad", "onCreate: liuyi 删除了一行log");
     }
 }
